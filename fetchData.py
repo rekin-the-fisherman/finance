1d2aed4f7b48afc4898bc23d478f34b7442bd5b4
Jomar <aletajomar02221@gmail.com>
2026-07-16 18:37:57 +0800

    anti duplicate feature

--- fetchData.py
+++ fetchData.py
@@ -1,10 +1,12 @@
 import yfinance as yf
+import pandas as pd
 from sqlalchemy import create_engine
 
 dbConnectionString = "mysql+pymysql://root:@localhost:3306/project_finance"
-
-
 engine = create_engine(dbConnectionString)
+
+
+existingDates = pd.read_sql("SELECT date FROM stock_prices WHERE ticker = 'AAPL'", engine)
 
 
 data = yf.download('AAPL')
@@ -15,5 +17,13 @@ data = data.reset_index()
 
 data['ticker'] = "AAPL"
 
+existingDates['date'] = pd.to_datetime(existingDates['date'])
+
+data = data[~data['Date'].isin(existingDates['date'])]
+
+print(f"Inserting {len(data)} new rows")
+
 data.to_sql('stock_prices', engine, if_exists='append', index=False)
+print(existingDates)
+
             
