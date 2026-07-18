9903f311925941b3b0e0a229998e0cb542e1c1bc
Jomar <aletajomar02221@gmail.com>
2026-07-18 16:04:41 +0800

    error handling

--- dashboard.js
+++ dashboard.js
@@ -2,11 +2,17 @@
 function getMetrics() {
     const ticker = document.getElementById("tickerInput").value;
     fetch(`http://127.0.0.1:8000/metrics/${ticker}`)
+    
         .then(response => response.json())
-        .then(data => document.getElementById("results").innerHTML = `
-            <p>Sharpe Ratio: ${data.sharpe_ratio}</p>
-            <p>Annualized Volatility: ${data.annualized_volatility}</p>
-            <p>Annualized Mean: ${data.annualized_mean}</p>
-            <p>VaR: ${data.var}</p>
-    `)
+        .then(data => {
+            if (data.error) {
+                document.getElementById("results").innerHTML = `<p>${data.error}</p>`
+            } else {
+                document.getElementById("results").innerHTML = `
+                <p>Sharpe Ratio: ${data.sharpe_ratio.toFixed(2)}</p>
+                <p>Annualized Volatility: ${data.annualized_volatility.toFixed(2)}</p>
+                <p>Annualized Mean: ${data.annualized_mean.toFixed(2)}</p>
+                <p>VaR: ${data.var.toFixed(2)}</p>`
+            }
+    })
 }
