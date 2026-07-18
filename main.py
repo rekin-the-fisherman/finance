0c8d846cf143b82c8589cd6cb9c4be792289403d
Jomar <aletajomar02221@gmail.com>
2026-07-18 15:29:42 +0800

    updated

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import calculateMetrics as cm
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
